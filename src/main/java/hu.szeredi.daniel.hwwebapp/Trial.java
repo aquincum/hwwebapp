62051b59cc9c4c82a87d651b5e2b9dea2c7ad253
Daniel Szeredi <daniel@szeredi.hu>
2016-12-14 15:16:42 -0500

    p2

--- src/main/java/hu.szeredi.daniel.hwwebapp/Trial.java
+++ src/main/java/hu.szeredi.daniel.hwwebapp/Trial.java
@@ -3,6 +3,8 @@ package hu.szeredi.daniel.hwwebapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +18,14 @@ public class Trial {
     String home(){
         return "I'm in your applicatioun";
     }
+
+    @RequestMapping("/")
+    @ResponseBody
+    String asdhome(){
+        return "Isadasdasdas'm in your applicatioun";
+    }
+
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Trial.class, args);
---
+++ src/main/java/hu.szeredi.daniel.hwwebapp/WebInitializer.java
@@ -0,0 +1,12 @@
+package hu.szeredi.daniel.hwwebapp;
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class WebInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Trial.class);
+    }
+}
