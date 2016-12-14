f9641b9c27b97957caa690e7589468b743a88237
Daniel Szeredi <daniel@szeredi.hu>
2016-12-14 14:45:33 -0500

    Spring boot?

---
+++ src/main/java/hu.szeredi.daniel.hwwebapp/Trial.java
@@ -0,0 +1,22 @@
+package hu.szeredi.daniel.hwwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class Trial {
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "I'm in your applicatioun";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Trial.class, args);
+    }
+
+}
